6252385b79fa3f05350078e9045b15a355f41743
Srikote Naewchampa <whcwhc78@gmail.com>
2019-04-06 08:24:36 +0700

    add mapper structs

---
+++ mapper/index.go
@@ -0,0 +1,31 @@
+package mapper
+
+type University struct {
+	id   string
+	name string
+}
+
+type Course struct {
+	id      string
+	name    string
+	faculty string
+	project string
+}
+
+type Student struct {
+	applicationId    string
+	citizenId        string
+	title            string
+	firstName        string
+	lastName         string
+	phone            string
+	email            string
+	appliedCourseIds [6]string
+}
+
+type Ranking struct {
+	courseId  string
+	citizenId string
+	rank      uint8
+	round     uint8
+}
