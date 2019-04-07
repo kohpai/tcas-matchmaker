142ffcc31a335a31fc5031c7db8214ba5c6d9e97
Srikote Naewchampa <whcwhc78@gmail.com>
2019-04-07 08:43:23 +0700

    add course Apply method

--- model/course/course.go
+++ model/course/course.go
@@ -1,16 +1,25 @@
 package course
 
+import (
+	"log"
+
+	"github.com/kohpai/tcas-3rd-round-resolver/model/student"
+)
+
 type Course struct {
 	id          string
 	isFull      bool
 	jointCourse *JointCourse
+	students    []*student.Student
 }
 
 func NewCourse(id string, jointCourse *JointCourse) *Course {
+	students := make([]*student.Student, 0)
 	course := &Course{
 		id,
 		false,
 		jointCourse,
+		students,
 	}
 
 	if jointCourse.availableSpots == 0 {
@@ -20,4 +29,18 @@ func NewCourse(id string, jointCourse *JointCourse) *Course {
 	jointCourse.courses = append(jointCourse.courses, course)
 
 	return course
+}
+
+func (course *Course) Apply(s *student.Student) bool {
+	if course.isFull {
+		return false
+	}
+
+	// @ASSERTION, this shouldn't happen
+	if !course.jointCourse.Apply() {
+		log.Println("Apply returns false")
+	}
+
+	course.students = append(course.students, s)
+	return true
 }
--- model/course/course_test.go
+++ model/course/course_test.go
@@ -1,6 +1,10 @@
 package course
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/kohpai/tcas-3rd-round-resolver/model/student"
+)
 
 func TestNewCourse_Always_ReturnsCourse(t *testing.T) {
 	jointCourse := NewJointCourse("1234", 10)
@@ -41,5 +45,49 @@ func TestIsFull_AvailableSpotsIsAlreadyZero_ReturnsTrue(t *testing.T) {
 
 	if !course.isFull {
 		t.Error("Course is NOT full", course)
+	}
+}
+
+func TestApply_CourseIsNotFull_ReturnsTrue(t *testing.T) {
+	jointCourse := NewJointCourse("1234", 1)
+	course := NewCourse("1234", jointCourse)
+
+	s := student.NewStudent("1349")
+
+	if !course.Apply(s) {
+		t.Error("Apply returns false", course)
+	}
+}
+
+func TestApply_OneSpotLeft_CourseIsFull(t *testing.T) {
+	jointCourse := NewJointCourse("1234", 1)
+	course := NewCourse("1234", jointCourse)
+
+	s := student.NewStudent("1349")
+	course.Apply(s)
+
+	if !course.isFull {
+		t.Error("Course is NOT full", course)
+	}
+}
+
+func TestApply_MoreSpotsLeft_StudentsAreEnrolled(t *testing.T) {
+	jointCourse := NewJointCourse("1234", 3)
+	course := NewCourse("1234", jointCourse)
+
+	ss := []*student.Student{
+		student.NewStudent("1349"),
+		student.NewStudent("1350"),
+		student.NewStudent("1351"),
+	}
+
+	for _, s := range ss {
+		course.Apply(s)
+	}
+
+	for i, s := range course.students {
+		if s != ss[i] {
+			t.Errorf("Student is not enrolled, %d", i)
+		}
 	}
 }
