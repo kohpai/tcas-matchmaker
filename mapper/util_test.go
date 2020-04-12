77a83ff824844b51c2c32eba7c7f9da3a9fafa3f
Kohpai <whcwhc78@gmail.com>
2020-04-12 17:25:43 +0700

    :sparkles: update test

--- mapper/util_test.go
+++ mapper/util_test.go
@@ -4,77 +4,100 @@ import (
 	"testing"
 )
 
-func TestCreateRankingMap_Always_ReturnRankingMap(t *testing.T) {
-	rankings := []Ranking{
+func TestExtractRankings_Always_ReturnRankingMap(t *testing.T) {
+	apps := []Application{
 		{
 			CourseId:  "1234",
 			CitizenId: "13499",
-			Rank:      1,
+			Ranking:   1,
 		},
 		{
 			CourseId:  "1234",
 			CitizenId: "13501",
-			Rank:      2,
+			Ranking:   2,
 		},
 		{
 			CourseId:  "1234",
 			CitizenId: "13502",
-			Rank:      3,
+			Ranking:   3,
 		},
 		{
 			CourseId:  "1235",
 			CitizenId: "13500",
-			Rank:      2,
+			Ranking:   2,
 		},
 		{
 			CourseId:  "1236",
 			CitizenId: "13499",
-			Rank:      1,
+			Ranking:   1,
 		},
 		{
 			CourseId:  "1237",
 			CitizenId: "13500",
-			Rank:      1,
+			Ranking:   1,
 		},
 		{
 			CourseId:  "1237",
 			CitizenId: "13499",
-			Rank:      2,
+			Ranking:   2,
 		},
 	}
 
-	rankingInfoMap := ExtractRankings(rankings)
-	rankingMap := createRankingMap(rankingInfoMap)
+	rankingMap := ExtractRankings(apps)
 
 	if rank := rankingMap["1234"]["13499"]; rank != 1 {
-		t.Error("Rank is incorrect, got", rank, rankings[0])
+		t.Error("Rank is incorrect, got", rank, apps[0])
 	}
 	if rank := rankingMap["1234"]["13501"]; rank != 2 {
-		t.Error("Rank is incorrect, got", rank, rankings[1])
+		t.Error("Rank is incorrect, got", rank, apps[1])
 	}
 	if rank := rankingMap["1234"]["13502"]; rank != 3 {
-		t.Error("Rank is incorrect, got", rank, rankings[2])
+		t.Error("Rank is incorrect, got", rank, apps[2])
 	}
 	if rank := rankingMap["1235"]["13500"]; rank != 2 {
-		t.Error("Rank is incorrect, got", rankings[3])
+		t.Error("Rank is incorrect, got", apps[3])
 	}
 	if rank := rankingMap["1236"]["13499"]; rank != 1 {
-		t.Error("Rank is incorrect, got", rank, rankings[4])
+		t.Error("Rank is incorrect, got", rank, apps[4])
 	}
 	if rank := rankingMap["1237"]["13500"]; rank != 1 {
-		t.Error("Rank is incorrect, got", rank, rankings[5])
+		t.Error("Rank is incorrect, got", rank, apps[5])
 	}
 	if rank := rankingMap["1237"]["13499"]; rank != 2 {
-		t.Error("Rank is incorrect, got", rank, rankings[6])
+		t.Error("Rank is incorrect, got", rank, apps[6])
 	}
 }
 
 func TestCreateJointCourseMap_Always_ReturnsJointCourseMap(t *testing.T) {
 	courses := []Course{
-		{"1234", "", 10, "1", 0},
-		{"1235", "", 11, "1", 0},
-		{"1236", "123", 12, "1", 0},
-		{"1237", "123", 12, "1", 0},
+		{
+			CourseId:            "1234",
+			UniversityId:        "4321",
+			JointId:             "",
+			ReceiveAmount:       10,
+			ExceedAllowedAmount: "A",
+		},
+		{
+			CourseId:            "1235",
+			UniversityId:        "4321",
+			JointId:             "",
+			ReceiveAmount:       11,
+			ExceedAllowedAmount: "A",
+		},
+		{
+			CourseId:            "1236",
+			UniversityId:        "4322",
+			JointId:             "123",
+			ReceiveAmount:       12,
+			ExceedAllowedAmount: "A",
+		},
+		{
+			CourseId:            "1237",
+			UniversityId:        "4322",
+			JointId:             "123",
+			ReceiveAmount:       12,
+			ExceedAllowedAmount: "A",
+		},
 	}
 
 	jointCourseMap := createJointCourseMap(courses)
@@ -87,59 +110,83 @@ func TestCreateJointCourseMap_Always_ReturnsJointCourseMap(t *testing.T) {
 		t.Error("Available spots is incorrect", jc)
 	}
 
-	if jc := jointCourseMap["123"]; jc.AvailableSpots() != 12 {
+	if jc := jointCourseMap["4322123"]; jc.AvailableSpots() != 12 {
 		t.Error("Available spots is incorrect", jc)
 	}
 }
 
 func TestCreateCourseMap_Always_ReturnsCourseMap(t *testing.T) {
-	rankings := []Ranking{
+	apps := []Application{
 		{
 			CourseId:  "1234",
 			CitizenId: "13499",
-			Rank:      1,
+			Ranking:   1,
 		},
 		{
 			CourseId:  "1234",
 			CitizenId: "13501",
-			Rank:      2,
+			Ranking:   2,
 		},
 		{
 			CourseId:  "1234",
 			CitizenId: "13502",
-			Rank:      3,
+			Ranking:   3,
 		},
 		{
 			CourseId:  "1235",
 			CitizenId: "13500",
-			Rank:      2,
+			Ranking:   2,
 		},
 		{
 			CourseId:  "1236",
 			CitizenId: "13499",
-			Rank:      1,
+			Ranking:   1,
 		},
 		{
 			CourseId:  "1237",
 			CitizenId: "13500",
-			Rank:      1,
+			Ranking:   1,
 		},
 		{
 			CourseId:  "1237",
 			CitizenId: "13499",
-			Rank:      2,
+			Ranking:   2,
 		},
 	}
-	rankingInfoMap := ExtractRankings(rankings)
+	rankingMap := ExtractRankings(apps)
 
 	courses := []Course{
-		{"1234", "", 10, "1", 0},
-		{"1235", "", 11, "1", 0},
-		{"1236", "123", 12, "1", 0},
-		{"1237", "123", 12, "1", 0},
+		{
+			CourseId:            "1234",
+			UniversityId:        "4321",
+			JointId:             "",
+			ReceiveAmount:       10,
+			ExceedAllowedAmount: "A",
+		},
+		{
+			CourseId:            "1235",
+			UniversityId:        "4321",
+			JointId:             "",
+			ReceiveAmount:       11,
+			ExceedAllowedAmount: "A",
+		},
+		{
+			CourseId:            "1236",
+			UniversityId:        "4322",
+			JointId:             "123",
+			ReceiveAmount:       12,
+			ExceedAllowedAmount: "A",
+		},
+		{
+			CourseId:            "1237",
+			UniversityId:        "4322",
+			JointId:             "123",
+			ReceiveAmount:       12,
+			ExceedAllowedAmount: "A",
+		},
 	}
 
-	courseMap := CreateCourseMap(courses, rankingInfoMap)
+	courseMap := CreateCourseMap(courses, rankingMap)
 
 	if course := courseMap["1234"]; course.JointCourse().AvailableSpots() != 10 {
 		t.Error("Available spots is incorrect", course)
@@ -169,58 +216,84 @@ func TestCreateCourseMap_Always_ReturnsCourseMap(t *testing.T) {
 }
 
 func TestCreateStudentMap_Always_ReturnsStudentMap(t *testing.T) {
-	rankings := []Ranking{
+	apps := []Application{
 		{
 			CourseId:  "1234",
 			CitizenId: "13499",
-			Rank:      1,
+			Priority:  1,
+			Ranking:   1,
 		},
 		{
 			CourseId:  "1234",
 			CitizenId: "13501",
-			Rank:      2,
+			Priority:  1,
+			Ranking:   2,
 		},
 		{
 			CourseId:  "1234",
 			CitizenId: "13502",
-			Rank:      3,
+			Priority:  1,
+			Ranking:   3,
 		},
 		{
 			CourseId:  "1235",
 			CitizenId: "13500",
-			Rank:      2,
+			Priority:  1,
+			Ranking:   2,
 		},
 		{
 			CourseId:  "1236",
 			CitizenId: "13499",
-			Rank:      1,
+			Priority:  2,
+			Ranking:   1,
 		},
 		{
 			CourseId:  "1237",
 			CitizenId: "13500",
-			Rank:      1,
+			Priority:  3,
+			Ranking:   1,
 		},
 		{
 			CourseId:  "1237",
 			CitizenId: "13499",
-			Rank:      2,
+			Priority:  4,
+			Ranking:   2,
 		},
 	}
 	courses := []Course{
-		{"1234", "", 10, "1", 0},
-		{"1235", "", 11, "1", 0},
-		{"1236", "123", 12, "1", 0},
-		{"1237", "123", 12, "1", 0},
-	}
-	students := []Student{
-		{"13499", "1234", 1},
-		{"13499", "1236", 2},
-		{"13500", "1237", 3},
+		{
+			CourseId:            "1234",
+			UniversityId:        "4321",
+			JointId:             "",
+			ReceiveAmount:       10,
+			ExceedAllowedAmount: "A",
+		},
+		{
+			CourseId:            "1235",
+			UniversityId:        "4321",
+			JointId:             "",
+			ReceiveAmount:       11,
+			ExceedAllowedAmount: "A",
+		},
+		{
+			CourseId:            "1236",
+			UniversityId:        "4322",
+			JointId:             "123",
+			ReceiveAmount:       12,
+			ExceedAllowedAmount: "A",
+		},
+		{
+			CourseId:            "1237",
+			UniversityId:        "4322",
+			JointId:             "123",
+			ReceiveAmount:       12,
+			ExceedAllowedAmount: "A",
+		},
 	}
 
-	rankingInfoMap := ExtractRankings(rankings)
-	courseMap := CreateCourseMap(courses, rankingInfoMap)
-	studentMap := CreateStudentMap(students, courseMap)
+	rankingMap := ExtractRankings(apps)
+	courseMap := CreateCourseMap(courses, rankingMap)
+	studentMap := CreateStudentMap(apps, courseMap)
 
 	student1, student2 := studentMap["13499"], studentMap["13500"]
 	if course, err := student1.PreferredCourse(1); err != nil || course.Id() != "1234" {
