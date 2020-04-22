b735c81c7673f5aa6f403dca46e0fd74073ed0c8
Kohpai <whcwhc78@gmail.com>
2020-04-22 23:24:48 +0700

    :sparkles: add female test

--- model/strategy_test.go
+++ model/strategy_test.go
@@ -224,7 +224,7 @@ func TestApply_AllowSomeExceedLimit_AdmitNone(t *testing.T) {
 	}
 }
 
-func TestApply_Gender1_NoDuplicatedStudentsAreAdmitted(t *testing.T) {
+func TestApply_Male_NoDuplicatedStudentsAreAdmitted(t *testing.T) {
 	strategy := NewApplyStrategy("", 0)
 	availableSpots := NewAvailableSpots(6, 3, 0, 0, 0, 0, 0)
 	jointCourse := NewJointCourse("1234", availableSpots, strategy)
@@ -246,6 +246,39 @@ func TestApply_Gender1_NoDuplicatedStudentsAreAdmitted(t *testing.T) {
 		NewStudent("1350", genders.Male),
 		NewStudent("1351", genders.Female),
 		NewStudent("1352", genders.Female),
+	}
+
+	for _, s := range ss {
+		course.Apply(s)
+	}
+
+	if pq := jointCourse.Students(); pq.Len() != 3 {
+		t.Error("Wrong number of students", pq.Students())
+	}
+}
+
+func TestApply_Female_NoDuplicatedStudentsAreAdmitted(t *testing.T) {
+	strategy := NewApplyStrategy("", 0)
+	availableSpots := NewAvailableSpots(6, 0, 3, 0, 0, 0, 0)
+	jointCourse := NewJointCourse("1234", availableSpots, strategy)
+	ranking := Ranking{
+		"1352": 3,
+		"1351": 3,
+		"1350": 2,
+		"1349": 2,
+		"1348": 2,
+		"1347": 1,
+	}
+	course := NewCourse("1234", jointCourse, ranking)
+
+	genders := Genders()
+	ss := []*Student{
+		NewStudent("1347", genders.Female),
+		NewStudent("1348", genders.Female),
+		NewStudent("1349", genders.Female),
+		NewStudent("1350", genders.Female),
+		NewStudent("1351", genders.Male),
+		NewStudent("1352", genders.Male),
 	}
 
 	for _, s := range ss {
